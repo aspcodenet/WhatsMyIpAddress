7e0ed9eb6d883c566f65a0cf1b95348594166f09
Stefan Holmberg <stefan.holmberg@systementor.se>
2020-04-15 11:39:39 +0200

    Add project files.

---
+++ WhatsMyIpAddress/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.IISIntegration;
+using Microsoft.Extensions.Logging;
+using WhatsMyIpAddress.Models;
+using WhatsMyIpAddress.Services;
+
+namespace WhatsMyIpAddress.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IGetIPService _ipService;
+
+        public HomeController(ILogger<HomeController> logger, IGetIPService ipService)
+        {
+            _logger = logger;
+            _ipService = ipService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        { 
+            return View();
+        }
+
+        public IActionResult IP()
+        {
+            var ip = _ipService.GetIpAddress();
+            return View("IP", ip);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WhatsMyIpAddress/Services/IGetIPService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace WhatsMyIpAddress.Services
+{
+    public interface IGetIPService
+    {
+        string GetIpAddress();
+    }
+
+    class GetIpService : IGetIPService
+    {
+        private readonly IActionContextAccessor _accessor;
+
+        public GetIpService(IActionContextAccessor accessor)
+        {
+            _accessor = accessor;
+        }
+        public string GetIpAddress()
+        {
+            var context = _accessor.ActionContext.HttpContext;
+           
+            return context.Connection.RemoteIpAddress.ToString();
+        }
+    }
+}
